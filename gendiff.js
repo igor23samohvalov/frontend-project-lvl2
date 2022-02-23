5cd12992d9b36b2daaeb8ebcbc9ba19029529442
igor samokhvalov <sandypandaadv@outlook.com>
2022-02-23 11:57:53 +0300

    addition of gendiff.js as a program for command line, makefile initialization, addition of gitignore and switching in package.json to type - module

---
+++ gendiff.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1');
+
+// program.command('split')
+//   .description('Compares two configuration files and shows a difference.')
+//   .option('-V, --version', 'output the version number')
+//   .option('-h, --help', 'output usage information')
+
+
+program.parse();
