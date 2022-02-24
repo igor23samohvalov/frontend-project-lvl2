a75848b51f12f2ab4762b60993f4808596515d30
igor samokhvalov <sandypandaadv@outlook.com>
2022-02-24 15:38:14 +0300

    gendiff compares 2 arguments and console.logs the result, addition of eslint and a dir for fixtures

--- gendiff.js
+++ gendiff.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import _ from 'lodash';
+import * as fs from 'fs';
+import { cwd } from 'process';
+import path from 'path';
 
 const program = new Command();
 
@@ -8,6 +12,48 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>')
+  .action((f1, f2, options) => {
+    const firstFile = JSON.parse(fs.readFileSync(path.resolve(cwd(), f1)));
+    const secondFile = JSON.parse(fs.readFileSync(path.resolve(cwd(), f2)));
+
+    console.log(genDiff(firstFile,secondFile));
+
+    if (options.format) {
+      console.log(f1.split('.')[1] )
+    }
+  })
 
 program.parse();
+
+function genDiff(file1, file2) {
+	const file2Data = Object.entries(file2);
+  const file1Data = Object.entries(file1);
+  const result = [];
+
+	for (let [key, value] of file2Data) {
+  	if (file1.hasOwnProperty(key)) {
+    	if (file1[key] === value) {
+      	result.push(`    ${key}: ${value}`)
+      } else {
+      	result.push(`  - ${key}: ${file1[key]}`)
+        result.push(`  + ${key}: ${value}`)
+      }
+    } else {
+    	result.push(`  + ${key}: ${value}`);
+    }
+  }
+  for (let [key, value] of file1Data) {
+  	if (!file2.hasOwnProperty(key)) {
+    	result.push((`  - ${key}: ${value}`))
+    }
+  }
+
+	const sortedResult = _.sortBy(result, (item) => item.slice(4, 5));
+  sortedResult.push('}');
+  sortedResult.unshift('{')
+
+  return sortedResult.join('\n');
+}
+
+export default genDiff;
