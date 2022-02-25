e35032cf2bac48191f4dc420a8af75f61374a2e6
igor samokhvalov <sandypandaadv@outlook.com>
2022-02-25 12:04:30 +0300

    adding codeclimate, updating on makefile, code update for eslint terms

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/igor23samohvalov/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/igor23samohvalov/frontend-project-lvl2/actions)
 [![asciicast](https://asciinema.org/a/xrd3b98qTDovGtXdfwf5XwBen.svg)](https://asciinema.org/a/xrd3b98qTDovGtXdfwf5XwBen)
+[![Maintainability](https://api.codeclimate.com/v1/badges/6d0874f3986e54b2fdc9/maintainability)](https://codeclimate.com/github/igor23samohvalov/frontend-project-lvl2/maintainability)
--- gendiff.js
+++ gendiff.js
@@ -7,6 +7,37 @@ import path from 'path';
 
 const program = new Command();
 
+function genDiff(file1, file2) {
+  const file2Data = Object.entries(file2);
+  const file1Data = Object.entries(file1);
+  const result = [];
+
+  file2Data.forEach(([key, value]) => {
+    if (Object.prototype.hasOwnProperty.call(file1, key)) {
+      if (file1[key] === value) {
+        result.push(`    ${key}: ${value}`);
+      } else {
+        result.push(`  - ${key}: ${file1[key]}`);
+        result.push(`  + ${key}: ${value}`);
+      }
+    } else {
+      result.push(`  + ${key}: ${value}`);
+    }
+  });
+
+  file1Data.forEach(([key, value]) => {
+    if (!Object.prototype.hasOwnProperty.call(file2, key)) {
+      result.push((`  - ${key}: ${value}`));
+    }
+  });
+
+  const sortedResult = _.sortBy(result, (item) => item.slice(4, 5));
+  sortedResult.push('}');
+  sortedResult.unshift('{');
+
+  return sortedResult.join('\n');
+}
+
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
@@ -17,43 +48,13 @@ program
     const firstFile = JSON.parse(fs.readFileSync(path.resolve(cwd(), f1)));
     const secondFile = JSON.parse(fs.readFileSync(path.resolve(cwd(), f2)));
 
-    console.log(genDiff(firstFile,secondFile));
+    console.log(genDiff(firstFile, secondFile));
 
     if (options.format) {
-      console.log(f1.split('.')[1] )
+      console.log(f1.split('.')[1]);
     }
-  })
+  });
 
 program.parse();
-
-function genDiff(file1, file2) {
-	const file2Data = Object.entries(file2);
-  const file1Data = Object.entries(file1);
-  const result = [];
-
-	for (let [key, value] of file2Data) {
-  	if (file1.hasOwnProperty(key)) {
-    	if (file1[key] === value) {
-      	result.push(`    ${key}: ${value}`)
-      } else {
-      	result.push(`  - ${key}: ${file1[key]}`)
-        result.push(`  + ${key}: ${value}`)
-      }
-    } else {
-    	result.push(`  + ${key}: ${value}`);
-    }
-  }
-  for (let [key, value] of file1Data) {
-  	if (!file2.hasOwnProperty(key)) {
-    	result.push((`  - ${key}: ${value}`))
-    }
-  }
-
-	const sortedResult = _.sortBy(result, (item) => item.slice(4, 5));
-  sortedResult.push('}');
-  sortedResult.unshift('{')
-
-  return sortedResult.join('\n');
-}
 
 export default genDiff;
